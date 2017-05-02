9358dcdbb84ddaa36dac85e8081b117fa568163d
Daniel Futrillé <futridan@gmail.com>
2017-05-01 20:07:13 -0400

    [Laravel Seeder]
    Were included Users and Payments

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Cliente;
+use App\Pago;
+use App\Model\PagoHistorico;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,20 +31,75 @@ class Tmc_usersSeeder extends Seeder {
      */
     public function run()
     {
+        $nuevos = [
+            ['nombre' => 'Daniel Futrille', 'email' => 'futridan@gmail.com'],
+            ['nombre' => 'Administrador', 'email' => 'admin@gmail.com'],
+            ['nombre' => 'Procesador Pagos', 'email' => 'procesador@gmail.com'],
+        ];
+        $products = [
+            'Caja de Naranjas','Mandarinas','Cambures','Pepitos','Monitor 17 Pulgadas','Teclado','Mouse Optico',
+            'Mueble Decorativo','Moto','Laptod','Almuerzo','Desayuno','Cena','Disco Duro','Celular Inteligente',
+            'Impresora Multifuncional','Charcutería','Queso Amarillo','Jamon Ahumado','Galletas','Pollo Entero',
+        ];
+
+        $nombres = [
+            'Martin','María','Andrea','Martha','Andres','Carlos','Miguel','Karla','Mercedes','Milagros',
+            'Daniela','Freddy','Jhony','Wikelan','Yefferson','Yuleidy','Lola','Sinibaldo','Marina','Eduardo',
+            'Luis','Migueangel',
+        ];
+
+        $apellidos = [
+            'Martinez','Espinal','Perez','Fernandez','Futrille','Arcia','Maldonado','Perozo','Rodriguez','Garcia',
+            'Rojas','Coronado','Lopez','Vega','Paredes','Romero','Leon','Flores','Viera','Arguello','Villasmil',
+        ];
 
         if(DB::table('users')->count() == 0) {
-            $password = \Hash::make('123456');
-            $cms_users = DB::table('users')->insert(array(
-//                'id'                =>DB::table('users')->max('id')+1,
-                'usu_estatus' => User::$EST_ACTIVO,
-                'usu_nombre' => 'Daniel Futrille V',
-                'email'             => 'futridan@gmail.com',
-                'password'          => $password,
-                'usu_fechacreacion'          => date('Y-m-d H:i:s'),
-                'usu_fechaactualizacion'          => date('Y-m-d H:i:s'),
-            ));
+            foreach ($nuevos as $nuevo){
+                $user = User::create([
+                    'usu_estatus' => User::$EST_ACTIVO,
+                    'usu_nombre' => $nuevo['nombre'],
+                    'email' => $nuevo['email'],
+                    'password' => \Hash::make('123456'),
+                ]);
+
+                Cliente::create([
+                    'cli_nombre' => $user->usu_nombre,
+                    'cli_ci' => 'V' . rand(14, 19) . rand(100000, 999999),
+                    'cli_email' => $user->email,
+                    'usu_id' => $user->usu_id,
+                    'cli_direccionfiscal' => 'Dirección Fiscal',
+                    'cli_direccionenvio' => 'Dirección de envio para prueba',
+                    'cli_banco' => 'Banco Mercantil',
+                    'cli_nrocuenta' => rand(10000, 99999) . rand(10000, 99999) . rand(10000, 99999) . rand(10000, 99999),
+                    'cli_tipocuenta' => rand(1, 2),
+                ]);
+            }
         }
+        $users = DB::table('users')->get();
+        foreach($users as $usu){
+            for ($i = 0; $i < rand(5,20); $i++){
+                $payment = Pago::create([
+                    'usu_id' => $usu->usu_id,
+                    'pag_estatus' => Pago::$EST_PORPROCESAR,
+                    'pag_monto' => (rand(1,100)).(intval(rand(1,99)/10)*10).(intval(rand(10,99)/100)*100),
+                    'pag_concepto' => $products[rand(0,sizeof($products)-1)],
+                    'pag_nombretc' => $nombres[rand(0,sizeof($nombres)-1)].' '.$apellidos[rand(0,sizeof($apellidos)-1)],
+                    'pag_cith' => rand(3, 20).rand(100000, 999999),
+                    'pag_fechacreacion' => date('Y-m-'.rand(1,30).' '.rand(1,23).':i:s'),
+                ]);
+
+                $pgh = PagoHistorico::create([
+                    'pag_id' => $payment->pag_id,
+                    'pgh_columna' => 'pag_estatus',
+                    'pgh_valor' => $payment->pag_estatus,
+                    'usu_id' => $usu->usu_id,
+                ]);
+            }
+        }
+
+
 
     }
 
 }
+
